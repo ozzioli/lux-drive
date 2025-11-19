2df9afa9709c9cfad366b3a5716b1e8ae584ff3f
ThNs7 <thiagoo.nasci77@gmail.com>
2025-11-18 22:02:06 -0300

    feat: Login & Register Inicio

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,303 @@
 import 'package:flutter/material.dart';
+import 'home_page.dart'; // ← IMPORTE a HomePage
+import 'register_page.dart'; // ← IMPORTE a RegisterPage
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageLuxState();
+}
+
+class _LoginPageLuxState extends State<LoginPage> {
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+
+  String? errorMsg;
+
+  // Login local 
+  void loginLocal() {
+    final email = emailController.text.trim();
+    final pass = passController.text.trim();
+
+
+    if (email.isNotEmpty && pass.isNotEmpty) {
+      setState(() {
+        errorMsg = null;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Login realizado com sucesso!"),
+          backgroundColor: Colors.green,
+        ),
+      );
+
+      
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    } else {
+      setState(() {
+        errorMsg = "Preencha e-mail e senha";
+      });
+    }
+  }
+
+  void goToRegister() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const RegisterPage()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF0E0E0E),
+      body: Stack(
+        children: [
+          
+          Positioned.fill(
+            child: Container(
+              color: const Color(0xFF161616),
+            ),
+          ),
+
+          // FORMULÁRIO 
+          Positioned(
+            top: screenHeight * 0.25, 
+            left: screenWidth * 0.05, 
+            right: screenWidth * 0.05, 
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.05,
+                vertical: screenHeight * 0.04,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1E1E1E),
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: screenHeight * 0.03),
+
+                  // EMAIL INPUT
+                  TextField(
+                    controller: emailController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "E-mail",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.025), 
+
+                  // SENHA INPUT
+                  TextField(
+                    controller: passController,
+                    obscureText: true,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "Senha",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02), 
+
+                  // ESQUECEU A SENHA
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        // Adicionar ação para esqueceu a senha
+                      },
+                      child: Text(
+                        "Esqueceu a senha?",
+                        style: TextStyle(
+                          color: const Color(0xFFBEA264), 
+                          fontSize: screenWidth * 0.035, 
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02), 
+
+                  // BOTÃO LOGIN
+                  Center(
+                    child: Container(
+                      width: screenWidth * 0.4, 
+                      child: ElevatedButton(
+                        onPressed: loginLocal,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFBEA264),
+                          foregroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.018,
+                            horizontal: 20,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text(
+                          "Login", 
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.04, 
+                            fontWeight: FontWeight.bold,
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.01), 
+
+                  // DIVISOR "OU"
+                  Center(
+                    child: Text(
+                      "ou",
+                      style: TextStyle(
+                        color: Colors.grey.shade400,
+                        fontSize: screenWidth * 0.04, 
+                      ),
+                    ),
+                  ),
+
+                  // BOTÃO GOOGLE
+                  Center(
+                    child: TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.g_mobiledata, 
+                        color:Colors.white,
+                        size: screenWidth * 0.07,
+                      ),
+                      label: Text(
+                        "Entrar com o Google",
+                        style: TextStyle(
+                          color:Colors.white,
+                          fontSize: screenWidth * 0.048,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      style: TextButton.styleFrom(
+                        padding: EdgeInsets.symmetric(
+                          vertical: screenHeight * 0.012,
+                          horizontal: 20,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.01),
+
+                  // MENSAGEM DE ERRO
+                  if (errorMsg != null)
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        errorMsg!,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontSize: screenWidth * 0.045,
+                        ),
+                      ),
+                    ),
+
+                  SizedBox(height: screenHeight * 0.01),
+
+                  // LINK REGISTRAR
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Não possui uma conta? ",
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: screenWidth * 0.045, 
+                        ),
+                        children: [
+                          WidgetSpan(
+                            child: GestureDetector(
+                              onTap: goToRegister, 
+                              child: Text(
+                                "Registrar",
+                                style: TextStyle(
+                                  color: const Color(0xFFBEA264),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: screenWidth * 0.045,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // LOGO SOBREPONDO
+          Positioned(
+            top: screenHeight * 0.15, 
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Image.asset(
+                "assets/images/LogoLux.png",
+                height: screenHeight * 0.14, 
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+
+          // BOTÃO VOLTAR
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 10,
+            left: 10,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,10 +1,312 @@
 import 'package:flutter/material.dart';
+import 'package:luxdrive/pages/home_page.dart';
+import 'login_page.dart'; 
 
-class RegisterPage extends StatelessWidget {
+class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final cpfController = TextEditingController();
+  final nomeController = TextEditingController();
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
+  final confirmPassController = TextEditingController();
+
+  String? errorMsg;
+
+  // Função de registro
+  void register() {
+    final cpf = cpfController.text.trim();
+    final nome = nomeController.text.trim();
+    final email = emailController.text.trim();
+    final pass = passController.text.trim();
+    final confirmPass = confirmPassController.text.trim();
+
+    if (cpf.isEmpty || nome.isEmpty || email.isEmpty || pass.isEmpty || confirmPass.isEmpty) {
+      setState(() {
+        errorMsg = "Preencha todos os campos";
+      });
+    } else if (pass != confirmPass) {
+      setState(() {
+        errorMsg = "As senhas não coincidem";
+      });
+    } else {
+      setState(() {
+        errorMsg = null;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Cadastro realizado com sucesso!"),
+          backgroundColor: Colors.green,
+        ),
+      );
+       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+    }
+  }
+
+  void goToLogin() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF0E0E0E),
+      body: Stack(
+        children: [
+          // BACKGROUND 
+          Positioned.fill(
+            child: Container(
+              color: const Color(0xFF161616),
+            ),
+          ),
+
+          // FORMULÁRIO 
+          Positioned(
+            top: screenHeight * 0.20,
+            left: screenWidth * 0.05, 
+            right: screenWidth * 0.05, 
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.05,
+                vertical: screenHeight * 0.04,
+              ),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1E1E1E),
+                borderRadius: BorderRadius.circular(18),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(height: screenHeight * 0.03),
+
+                  // CPF INPUT
+                  TextField(
+                    controller: cpfController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "CPF",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // NOME DE USUÁRIO INPUT
+                  TextField(
+                    controller: nomeController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "Nome de usuário",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // EMAIL INPUT
+                  TextField(
+                    controller: emailController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "E-mail",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // SENHA INPUT
+                  TextField(
+                    controller: passController,
+                    obscureText: true,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "Senha",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // CONFIRMAR SENHA INPUT
+                  TextField(
+                    controller: confirmPassController,
+                    obscureText: true,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: const Color(0xFF1E1E1E),
+                      hintText: "Confirmar Senha",
+                      hintStyle: const TextStyle(color: Colors.white70),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(color: Color(0xFFBEA264)),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.03),
+
+                  // BOTÃO REGISTRAR
+                  Center(
+                    child: Container(
+                      width: screenWidth * 0.5, 
+                      child: ElevatedButton(
+                        onPressed: register,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFBEA264),
+                          foregroundColor: Colors.black,
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.018,
+                            horizontal: 20,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: Text(
+                          "Registrar", 
+                          style: TextStyle(
+                            fontSize: screenWidth * 0.04, 
+                            fontWeight: FontWeight.bold,
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // MENSAGEM DE ERRO
+                  if (errorMsg != null)
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        errorMsg!,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontSize: screenWidth * 0.035,
+                        ),
+                      ),
+                    ),
+
+                  SizedBox(height: screenHeight * 0.02),
+
+                  // LINK LOGIN
+                  Center(
+                    child: RichText(
+                      text: TextSpan(
+                        text: "Já possui uma conta? ",
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: screenWidth * 0.045, 
+                        ),
+                        children: [
+                          WidgetSpan(
+                            child: GestureDetector(
+                              onTap: goToLogin, 
+                              child: Text(
+                                "Fazer Login",
+                                style: TextStyle(
+                                  color: const Color(0xFFBEA264),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: screenWidth * 0.045,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+
+          // LOGO SOBREPONDO
+          Positioned(
+            top: screenHeight * 0.10, 
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Image.asset(
+                "assets/images/LogoLux.png",
+                height: screenHeight * 0.14, 
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+
+          // BOTÃO VOLTAR
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 10,
+            left: 10,
+            child: IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
